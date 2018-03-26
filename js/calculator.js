721492aee5759c4e35780f795261ba7e43a12dbc
jackson <jacksonhmteixeira@gmail.com>
2018-03-26 18:50:28 -0300

    atualização do projeto

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,63 @@
-var media = avarageFunction(56, 45,);
+var messageOfValidation = "Type only numbers";
 
-function avarageFunction(numberOne, numberTwo){
-    var avarage;
-    avarage = (numberOne + numberTwo)/2;
-    return avarage;
+
+// FUNCTION OF VALIDATION
+function validator() {
+    var firstNumber;
+    var secondNumber;
+
+    firstNumber = document.getElementById("firstNumber").value;
+    secondNumber = document.getElementById("secondNumber").value;
+
+    validateFirstNumber(firstNumber);
+    validadeSecondNumber(secondNumber);
 }
 
-document.getElementById('media').innerHTML = media;
+// FUNCTION OF VALIDATION TO FIRST NUMBER
+function validateFirstNumber(number) {
+    if (isNaN(number)) {
+        document.getElementById("messageOfValidationToFirstNumber").innerHTML = messageOfValidation;
+    } else {
+        document.getElementById("messageOfValidationToFirstNumber").innerHTML = "";
+    }
+}
+
+// FUNCTION OF VALIDATION TO SECOND NUMBER
+function validadeSecondNumber(number) {
+    if (isNaN(number)) {
+        document.getElementById("messageOfValidationToSecondNumber").innerHTML = messageOfValidation;
+    } else {
+        document.getElementById("messageOfValidationToSecondNumber").innerHTML = "";
+    }
+}
+
+// FUNCTION CALCULATE
+function calculate() {
+    var result;
+    var operator;
+    var firstNumber;
+    var secondNumber;
+
+    firstNumber = document.getElementById("firstNumber").value;
+    secondNumber = document.getElementById("secondNumber").value;
+    operator = document.getElementById("selectOperator").value;
+
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case "Select the Operator":
+            document.getElementById("result").innerHTML = "Não encontrou a operação";
+            break;
+    }
+    document.getElementById("result").innerHTML = firstNumber + operator + secondNumber + "=" + result;
+}
